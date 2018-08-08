707426827b004519812b5dfbd2cff09cda5ab0ac
Vijaya Prakash <mvjp.183@gmail.com>
2018-08-08 13:49:27 +0530

    Add files via upload
    Used C++11 and created 2 paths for image display and video playing.

--- videoStream/videoStream.cpp
+++ videoStream/videoStream.cpp
@@ -11,3 +11,20 @@ using namespace vjp;
             cvNamedWindow(MAIN_WINDOW_NAME, CV_WINDOW_AUTOSIZE); 
             cvMoveWindow(MAIN_WINDOW_NAME, MAIN_WINDOW_X_POS, MAIN_WINDOW_Y_POS);
 }
+
+void videoStream::playVideo(const char *videofile) {
+    setWindowSettings();
+    CvCapture *capture = cvCreateFileCapture(videofile);
+    IplImage *frame = NULL;
+    while(true){
+        frame = cvQueryFrame(capture);
+        if(!frame) break;
+        cvShowImage("Play Video", frame);
+        char c = cvWaitKey(33);
+        if (c==27) break;
+    }
+    cvReleaseCapture(&capture);
+   // cvDestroyWindow("Play Video");
+    
+    
+}
