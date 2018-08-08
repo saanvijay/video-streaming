1c6dac80d3fa04226b90d63f5b1bac9a80d3cce7
Vijaya Prakash <mvjp.183@gmail.com>
2018-08-08 13:50:04 +0530

    Add files via upload
    Used C++11 and created 2 paths for image display and video playing.

--- videoStream/videoStream.h
+++ videoStream/videoStream.h
@@ -5,23 +5,40 @@
 #include "opencv/cv.h"
 #include "opencv/highgui.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 #define MAIN_WINDOW_NAME "VideoStreamSample"
 #define MAIN_WINDOW_X_POS 100
 #define MAIN_WINDOW_Y_POS 100
 
+#define NO_OF_ARGS 2
+#define IMG_INPUT 1
+#define VIDEO_INPUT 2
+
 namespace vjp {
     
     class videoStream {
         
         IplImage* m_img; 
+        IplImage* m_video;
         
     public:
-    
+        videoStream():m_img(NULL),m_video(NULL) {}
+        ~videoStream() {}
+        videoStream(const videoStream&) = delete;
+        videoStream& operator=(const videoStream&) = delete;
+        
         void getImageData(int &width, int &height, int &channels);
         void setWindowSettings();
-        inline  void setVideoStreamImage(IplImage* img) { m_img = img;}
+        inline  void setVideoStreamImage(const char *imgFile) { 
+            m_img = cvLoadImage(imgFile);
+        }
         inline  IplImage* getVideoStreamImage() { return m_img; }
+        inline void displayImage() {
+            cvShowImage(MAIN_WINDOW_NAME,  m_img);
+            cvWaitKey(0);
+        }
+        void playVideo(const char*);
     };
 }
--- videoStream/videoStreamMain.cpp
+++ videoStream/videoStreamMain.cpp
@@ -4,38 +4,64 @@ using namespace vjp;
 int main(int argc, char *argv[])
 {
     int width, height, channels;
+    int input;
+    string videoFileName="";
+    string imgFileName="";
     
-    if(argc < 2){
-        printf("Usage: videoStramMain <image-file-name>\n");
-        exit(0);
+   // if(argc < NO_OF_ARGS ){
+      //  printf("Usage: videoStramMain <image-file-name> <video-file-name>\n");
+      //  exit(0);
+   // }
+    
+    std::cout << "OpenCV Samples" << std::endl;
+    std::cout << "---------------" << std::endl;
+    std::cout << "1. Display Image" << std::endl;
+    std::cout << "2. Play video from file" << std::endl;
+    std::cin >> input;
+    
+    if (input == IMG_INPUT) {
+        std::cout << "Enter Image file name " << std::endl;
+        std::cin >> imgFileName;
     }
+    else if (input == VIDEO_INPUT) {
+        std::cout << "Enter Video file name" << std::endl;
+        std::cin >> videoFileName;
+    }
+    else { 
+        std::cout << "Enter valid input" << std::endl;
+        exit(0); 
+        }
     
     // Create new instance for video stream class
     videoStream *vStream = new videoStream;
   
-
-  // Load Image and set videoStream pointer  
-  vStream->setVideoStreamImage(cvLoadImage(argv[1]));
+    if (input == VIDEO_INPUT && videoFileName != "") { 
+            vStream->playVideo(videoFileName.c_str()); 
+        }
+    else {
+        // Load Image and set videoStream pointer  
+        vStream->setVideoStreamImage(imgFileName.c_str());
   
-  if (!vStream->getVideoStreamImage())
-      std::cout << "Unable to load the image" << std::endl;
+  
+  
+        if (!vStream->getVideoStreamImage())
+            std::cout << "Unable to load the image" << std::endl;
       
-  // If image loaded properly then get the image details
-  vStream->getImageData(width, height, channels);
+        // If image loaded properly then get the image details
+        vStream->getImageData(width, height, channels);
 
   
-  std::cout << "Image Height = "<< height << std::endl;
-  std::cout << "Image Width  = "<< width << std::endl;
-  std::cout << "Image Channels = "<<channels << std::endl; 
+        std::cout << "Image Height = "<< height << std::endl;
+        std::cout << "Image Width  = "<< width << std::endl;
+        std::cout << "Image Channels = "<<channels << std::endl; 
 
-  // Window Settings
-  vStream->setWindowSettings();
+        // Window Settings
+        vStream->setWindowSettings();
 
-  // Image Display
-  cvShowImage(MAIN_WINDOW_NAME,  vStream->getVideoStreamImage());
-
-  // Key wait
-   cvWaitKey(0);
+        // Image Display
+        vStream->displayImage();
+    
+    }
 
   if (vStream) delete vStream;
     
